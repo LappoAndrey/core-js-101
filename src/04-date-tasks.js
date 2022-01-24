3b50369a6619d54b2b1ff0a231da868330c512c6
silent-pp <silent34@yandex.ru>
2022-01-24 22:47:23 +0300

    add date task
    add date task

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -6,7 +6,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Parses a rfc2822 string date representation into date value
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
@@ -19,8 +18,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,10 +33,9 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
-
 
 /**
  * Returns true if specified date is leap year and false otherwise
@@ -53,10 +51,18 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  if (date.getFullYear() % 4 !== 0) {
+    return false;
+  }
+  if (date.getFullYear() % 100 !== 0) {
+    return true;
+  }
+  if (date.getFullYear() % 400 !== 0) {
+    return false;
+  }
+  return true;
 }
-
 
 /**
  * Returns the string representation of the timespan between two dates.
@@ -73,10 +79,33 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const rez = new Date(endDate - startDate);
+  const h = rez.getUTCHours();
+  let hours = 0;
+  if (h < 10) {
+    hours = `0${h}`;
+  } else {
+    hours = h;
+  }
+  const m = rez.getUTCMinutes();
+  let minutes = 0;
+  if (m < 10) {
+    minutes = `0${m}`;
+  } else {
+    minutes = m;
+  }
+  const s = rez.getUTCSeconds();
+  let seconds = 0;
+  if (s < 10) {
+    seconds = `0${s}`;
+  } else {
+    seconds = s;
+  }
+  const ml = rez.getMilliseconds();
+  const mls = `${ml}${'0'.repeat(3 - ml.toString().length)}`;
+  return `${hours}:${minutes}:${seconds}.${mls}`;
 }
-
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,10 +123,19 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const time = new Date(date);
+  let hours = time.getUTCHours();
+  const minutes = time.getUTCMinutes();
+  if (hours > 12) {
+    hours -= 12;
+  }
+  let angle = Math.abs(hours * 30 - minutes * 6 + minutes / 2);
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+  return (angle * Math.PI) / 180;
 }
-
 
 module.exports = {
   parseDataFromRfc2822,
--- test.js
+++ test.js
@@ -1,20 +1,10 @@
-function swapHeadAndTail(arr) {
-  let middle = [];
-  if (arr.length <= 1) return arr;
-  if (arr.length % 2 === 0) {
-    middle = [];
-  } else {
-    middle = [arr[Math.floor(arr.length / 2)]];
-  }
-  if (middle.length !== 0) {
-    return arr
-      .slice(Math.floor(arr.length / 2) + 1)
-      .concat(middle)
-      .concat(arr.slice(0, Math.floor(arr.length / 2)));
-  }
-  return arr
-    .slice(Math.floor(arr.length / 2))
-    .concat(arr.slice(0, Math.floor(arr.length / 2)));
+function timeSpanToString(startDate, endDate) {
+  const rez = new Date(endDate - startDate);
+  console.log(startDate);
+  console.log(endDate);
+  console.log(rez);
+  console.log(rez.getHours());
+  return `${rez.getHours()}:${rez.getMinutes()}:${rez.getSeconds()}:${rez.getMilliseconds()}`;
 }
 
-console.log(swapHeadAndTail([1, 2, 3, 4, 5, 6, 7, 8]));
+timeSpanToString('2000, 1, 1, 10, 0, 0', '2000, 1, 1, 15, 20, 10, 453');
